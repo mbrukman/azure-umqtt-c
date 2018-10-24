788664de12fbcd1eee078b4f04986c5c14cca942
Jelani <jelani.brandon@microsoft.com>
2018-10-24 11:43:43 -0700

    Create mqtt integration test

--- tests/umqtt_client_int/umqtt_client_int.c
+++ tests/umqtt_client_int/umqtt_client_int.c
@@ -32,6 +32,11 @@ static void my_gballoc_free(void* ptr)
     free(ptr);
 }
 
+static void* my_gballoc_realloc(void* ptr, size_t size)
+{
+    return realloc(ptr, size);
+}
+
 #define ENABLE_MOCKS
 #include "azure_c_shared_utility/agenttime.h"
 #include "azure_c_shared_utility/threadapi.h"
@@ -40,11 +45,13 @@ static void my_gballoc_free(void* ptr)
 #include "azure_c_shared_utility/xio.h"
 #undef ENABLE_MOCKS
 
-#include "azure_umqtt_c/mqtt_client.h"
 #include "azure_umqtt_c/mqtt_message.h"
+#include "azure_umqtt_c/mqtt_codec.h"
+#include "azure_umqtt_c/mqtt_client.h"
 #include "azure_umqtt_c/mqttconst.h"
 
 #include "azure_c_shared_utility/strings.h"
+#include "azure_c_shared_utility/buffer_.h"
 
 #define ENABLE_MOCKS
 #include "azure_c_shared_utility/umock_c_prod.h"
@@ -69,6 +76,19 @@ extern "C"
 TEST_DEFINE_ENUM_TYPE(QOS_VALUE, QOS_VALUE_VALUES);
 IMPLEMENT_UMOCK_C_ENUM_TYPE(QOS_VALUE, QOS_VALUE_VALUES);
 
+typedef enum OPERATION_STATE_TAG
+{
+    OPERATION_STATE_NONE = 0x0000,
+    OPERATION_STATE_CONNECT = 0x0001,
+    OPERATION_STATE_SUBSCRIBE = 0x0002,
+    OPERATION_STATE_UNSUBSCRIBE = 0x0004,
+    OPERATION_STATE_PUBLISH = 0x0008,
+    OPERATION_STATE_PING = 0x00010,
+
+    OPERATION_STATE_TLS_CONNECT,
+
+} OPERATION_STATE;
+
 static const char* TEST_USERNAME = "testuser";
 static const char* TEST_PASSWORD = "testpassword";
 
@@ -86,24 +106,18 @@ static const int TEST_MSG_LEN = sizeof(TEST_MESSAGE)/sizeof(TEST_MESSAGE[0]);
 static const XIO_HANDLE TEST_IO_HANDLE = (XIO_HANDLE)0x11;
 static const uint16_t TEST_KEEP_ALIVE_INTERVAL = 20;
 static bool g_continue;
-
-typedef enum OPERATION_STATE_TAG
-{
-    OPERATION_STATE_CONNECT = 0x0001,
-    OPERATION_STATE_SUBSCRIBE = 0x0002,
-    OPERATION_STATE_UNSUBSCRIBE = 0x0004,
-    OPERATION_STATE_PUBLISH = 0x0008,
-    OPERATION_STATE_PING = 0x00010
-} OPERATION_STATE;
+static bool g_error_cb_invoked;
+static OPERATION_STATE g_current_state;
+static OPERATION_STATE g_fuzzy_operation;
 
 //ON_PACKET_COMPLETE_CALLBACK g_packetComplete;
-ON_IO_OPEN_COMPLETE g_openComplete;
-ON_BYTES_RECEIVED g_bytesRecv;
+ON_IO_OPEN_COMPLETE g_open_complete_cb;
+ON_BYTES_RECEIVED g_on_bytes_recv;
 ON_IO_ERROR g_ioError;
 ON_SEND_COMPLETE g_sendComplete;
-void* g_onCompleteCtx;
+void* g_open_complete_ctx;
 void* g_onSendCtx;
-void* g_bytesRecvCtx;
+void* g_bytes_rcv_ctx;
 void* g_ioErrorCtx;
 typedef struct TEST_COMPLETE_DATA_INSTANCE_TAG
 {
@@ -121,14 +135,22 @@ TEST_MUTEX_HANDLE test_serialize_mutex;
 extern "C" {
 #endif
 
+    static void fill_data_object(unsigned char* data, size_t length)
+    {
+        for (size_t index = 0; index < length; index++)
+        {
+            data[index] = (unsigned char)rand() % 128;
+        }
+    }
+
     static int my_xio_open(XIO_HANDLE handle, ON_IO_OPEN_COMPLETE on_io_open_complete, void* on_io_open_complete_context, ON_BYTES_RECEIVED on_bytes_received, void* on_bytes_received_context, ON_IO_ERROR on_io_error, void* on_io_error_context)
     {
         (void)handle;
         /* Bug? : This is a bit wierd, why are we not using on_io_error and on_bytes_received? */
-        g_openComplete = on_io_open_complete;
-        g_onCompleteCtx = on_io_open_complete_context;
-        g_bytesRecv = on_bytes_received;
-        g_bytesRecvCtx = on_bytes_received_context;
+        g_open_complete_cb = on_io_open_complete;
+        g_open_complete_ctx = on_io_open_complete_context;
+        g_on_bytes_recv = on_bytes_received;
+        g_bytes_rcv_ctx = on_bytes_received_context;
         g_ioError = on_io_error;
         g_ioErrorCtx = on_io_error_context;
         return 0;
@@ -141,7 +163,88 @@ extern "C" {
         (void)size;
         g_sendComplete = on_send_complete;
         g_onSendCtx = callback_context;
+        switch (g_current_state)
+        {
+            case OPERATION_STATE_TLS_CONNECT:
+                g_current_state = OPERATION_STATE_CONNECT;
+                break;
+            case OPERATION_STATE_CONNECT:
+                g_current_state = OPERATION_STATE_SUBSCRIBE;
+                break;
+            case OPERATION_STATE_SUBSCRIBE:
+            case OPERATION_STATE_UNSUBSCRIBE:
+            case OPERATION_STATE_PUBLISH:
+            case OPERATION_STATE_PING:
+                break;
+        }
+
         return 0;
+    }
+
+    static void my_xio_dowork(XIO_HANDLE xio)
+    {
+        (void)xio;
+        switch (g_current_state)
+        {
+            case OPERATION_STATE_TLS_CONNECT:
+                g_open_complete_cb(g_open_complete_ctx, IO_OPEN_OK);
+                break;
+            case OPERATION_STATE_CONNECT:
+            {
+                unsigned char CONNACK_RESP[] = { 0x20, 0x2, 0x1, 0x0 };
+                size_t length = sizeof(CONNACK_RESP) / sizeof(CONNACK_RESP[0]);
+                if (g_fuzzy_operation == g_current_state)
+                {
+                    fill_data_object(CONNACK_RESP+1, length-1);
+                }
+                g_on_bytes_recv(g_bytes_rcv_ctx, CONNACK_RESP, length);
+                break;
+            }
+            case OPERATION_STATE_SUBSCRIBE:
+            {
+                unsigned char SUBACK_RESP[] = { 0x90, 0x5, 0x12, 0x34, 0x01, 0x80, 0x02 };
+                size_t length = sizeof(SUBACK_RESP) / sizeof(SUBACK_RESP[0]);
+                if (g_fuzzy_operation == g_current_state)
+                {
+                    fill_data_object(SUBACK_RESP, length);
+                }
+                g_on_bytes_recv(g_bytes_rcv_ctx, SUBACK_RESP, length);
+                break;
+            }
+            case OPERATION_STATE_UNSUBSCRIBE:
+            {
+                unsigned char UNSUBACK_RESP[] = { 0xB0, 0x5, 0x12, 0x34, 0x01, 0x80, 0x02 };
+                size_t length = sizeof(UNSUBACK_RESP) / sizeof(UNSUBACK_RESP[0]);
+                if (g_fuzzy_operation == g_current_state)
+                {
+                    fill_data_object(UNSUBACK_RESP, length);
+                }
+                g_on_bytes_recv(g_bytes_rcv_ctx, UNSUBACK_RESP, length);
+                break;
+            }
+            case OPERATION_STATE_PUBLISH:
+            {
+                unsigned char PUBLISH[] = { 0x3F, 0x11, 0x00, 0x06, 0x54, 0x6f, 0x70, 0x69, 0x63, 0x12, 0x34, 0x64, 0x61, 0x74, 0x61, 0x20, 0x4d, 0x73, 0x67 };
+                size_t length = sizeof(PUBLISH) / sizeof(PUBLISH[0]);
+                if (g_fuzzy_operation == g_current_state)
+                {
+                    fill_data_object(PUBLISH, length);
+                }
+                g_on_bytes_recv(g_bytes_rcv_ctx, PUBLISH, length);
+                break;
+            }
+            case OPERATION_STATE_PING:
+            {
+                unsigned char PINGRESP_RESP[] = { 0xD0, 0x0 };
+                size_t length = sizeof(PINGRESP_RESP) / sizeof(PINGRESP_RESP[0]);
+                if (g_fuzzy_operation == g_current_state)
+                {
+                    fill_data_object(PINGRESP_RESP, length);
+                }
+                g_on_bytes_recv(g_bytes_rcv_ctx, PINGRESP_RESP, length);
+                break;
+            }
+        }
     }
 
     static int TEST_mallocAndStrcpy_s(char** destination, const char* source)
@@ -151,8 +254,6 @@ extern "C" {
         memcpy(*destination, source, src_len + 1);
         return 0;
     }
-
-
 #ifdef __cplusplus
 }
 #endif
@@ -205,9 +306,10 @@ static void error_cb(MQTT_CLIENT_HANDLE handle, MQTT_CLIENT_EVENT_ERROR error, v
         case MQTT_CLIENT_COMMUNICATION_ERROR:
         case MQTT_CLIENT_NO_PING_RESPONSE:
         case MQTT_CLIENT_UNKNOWN_ERROR:
-            //g_errorCallbackInvoked = true;
+            g_error_cb_invoked = true;
             break;
     }
+    g_continue = false;
 }
 
 static void setup_options(MQTT_CLIENT_OPTIONS* options, const char* clientId,
@@ -231,9 +333,23 @@ static void setup_options(MQTT_CLIENT_OPTIONS* options, const char* clientId,
     options->messageRetain = messageRetain;
 }
 
+static void setup_send_msg_mocks()
+{
+    EXPECTED_CALL(gballoc_malloc(IGNORED_NUM_ARG));
+    EXPECTED_CALL(gballoc_malloc(IGNORED_NUM_ARG));
+    EXPECTED_CALL(gballoc_malloc(IGNORED_NUM_ARG));
+    EXPECTED_CALL(mallocAndStrcpy_s(IGNORED_PTR_ARG, IGNORED_PTR_ARG));
+    EXPECTED_CALL(mallocAndStrcpy_s(IGNORED_PTR_ARG, IGNORED_PTR_ARG));
+    EXPECTED_CALL(mallocAndStrcpy_s(IGNORED_PTR_ARG, IGNORED_PTR_ARG));
+    EXPECTED_CALL(mallocAndStrcpy_s(IGNORED_PTR_ARG, IGNORED_PTR_ARG));
+    EXPECTED_CALL(mallocAndStrcpy_s(IGNORED_PTR_ARG, IGNORED_PTR_ARG));
+    EXPECTED_CALL(xio_open(IGNORED_PTR_ARG, IGNORED_PTR_ARG, IGNORED_PTR_ARG, IGNORED_PTR_ARG, IGNORED_PTR_ARG, IGNORED_PTR_ARG, IGNORED_PTR_ARG));
+}
+
 static void send_mqtt_msg(uint32_t op_state)
 {
     g_continue = true;
+    g_current_state = OPERATION_STATE_TLS_CONNECT;
 
     uint16_t packet_id = 4;
     int result;
@@ -266,6 +382,8 @@ static void send_mqtt_msg(uint32_t op_state)
 
         mqttmessage_destroy(handle);
     }
+    //const char* expected_call = umock_c_get_actual_calls();
+    //printf("Expected: %s", expected_call);
 
     do
     {
@@ -281,6 +399,8 @@ BEGIN_TEST_SUITE(umqtt_client_int)
 TEST_SUITE_INITIALIZE(suite_init)
 {
     int result;
+
+    srand((unsigned int)time(NULL));
 
     test_serialize_mutex = TEST_MUTEX_CREATE();
     ASSERT_IS_NOT_NULL(test_serialize_mutex);
@@ -310,9 +430,8 @@ TEST_SUITE_INITIALIZE(suite_init)
     REGISTER_GLOBAL_MOCK_HOOK(mallocAndStrcpy_s, TEST_mallocAndStrcpy_s);
 
     REGISTER_GLOBAL_MOCK_HOOK(gballoc_malloc, my_gballoc_malloc);
-    REGISTER_GLOBAL_MOCK_FAIL_RETURN(gballoc_malloc, NULL);
-
     REGISTER_GLOBAL_MOCK_HOOK(gballoc_free, my_gballoc_free);
+    REGISTER_GLOBAL_MOCK_HOOK(gballoc_realloc, my_gballoc_realloc);
 
     REGISTER_GLOBAL_MOCK_HOOK(xio_open, my_xio_open);
     REGISTER_GLOBAL_MOCK_FAIL_RETURN(xio_open, __FAILURE__);
@@ -320,11 +439,9 @@ TEST_SUITE_INITIALIZE(suite_init)
     REGISTER_GLOBAL_MOCK_FAIL_RETURN(xio_send, __FAILURE__);
     REGISTER_GLOBAL_MOCK_RETURN(xio_close, 0);
     REGISTER_GLOBAL_MOCK_FAIL_RETURN(xio_close, __FAILURE__);
+    REGISTER_GLOBAL_MOCK_HOOK(xio_dowork, my_xio_dowork);
 
     REGISTER_GLOBAL_MOCK_RETURN(get_time, time(NULL) );
-
-    //REGISTER_GLOBAL_MOCK_RETURN(platform_init, 0);
-    //REGISTER_GLOBAL_MOCK_FAIL_RETURN(platform_init, __FAILURE__);
 
     REGISTER_GLOBAL_MOCK_RETURN(mallocAndStrcpy_s, 0);
     REGISTER_GLOBAL_MOCK_FAIL_RETURN(mallocAndStrcpy_s, __FAILURE__);
@@ -342,6 +459,7 @@ TEST_FUNCTION_INITIALIZE(method_init)
     {
         ASSERT_FAIL("Could not acquire test serialization mutex.");
     }
+    g_error_cb_invoked = false;
 }
 
 TEST_FUNCTION_CLEANUP(method_cleanup)
@@ -364,11 +482,13 @@ static int should_skip_index(size_t current_index, const size_t skip_array[], si
     return result;
 }
 
-TEST_FUNCTION(fuzzing_CONNECT_succeeds)
+TEST_FUNCTION(umqtt_fuzzing_CONNECT_succeeds)
 {
     // arrange
+    g_fuzzy_operation = OPERATION_STATE_CONNECT;
 
-    send_mqtt_msg(0);
+    send_mqtt_msg(OPERATION_STATE_CONNECT);
+
     // assert
 }
 
